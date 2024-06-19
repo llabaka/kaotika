720b5d6b021fa50a25462f86771fb30e66dcef03
Oskar <oskar.calvo@aeg.eus>
2024-06-19 13:17:39 +0200

    Available for aeg.eus domain

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,7 +6,9 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      
+      authorizationParams: {
+        hd: "aeg.eus",
+      },
     }),
   ],
   
