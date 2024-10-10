b7629bf72dbf48a63b0a1a64149685c7e93cef4b
Oskar <oskar.calvo@aeg.eus>
2024-10-10 11:52:15 +0200

    [UPDATE] favicon and pages menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import { AppProps } from 'next/app';
 import "../styles/globals.css";
 import {NextUIProvider} from "@nextui-org/react";
+import Head from "next/head";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,6 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <NextUIProvider>
+          <Head>
+            <link rel='icon' href='/favicon.ico' />
+          </Head>
           <Component {...pageProps} />
       </NextUIProvider>
     </SessionProvider>
