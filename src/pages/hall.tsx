1ce6d79df6672e82d896e7f42f293bdeaa066d05
Oskar <oskar.calvo@aeg.eus>
2024-10-10 12:04:26 +0200

    [UPDATE] hall of fame

--- src/pages/hall.tsx
+++ src/pages/hall.tsx
@@ -5,6 +5,7 @@ import Layout from '@/components/Layout';
 import Loading from '@/components/Loading';
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/table';
 import {Player} from '../_common/interfaces/Player';
+import { Tooltip } from '@nextui-org/react';
 
 const Hall = () => {
   const { data: session } = useSession();
@@ -65,15 +66,15 @@ const Hall = () => {
                 <TableCell className="text-center"><div className="flex justify-center"><Image src={player.avatar} alt="User Avatar" width={64} height={64} className="sepia rounded-full" /></div></TableCell>
                 <TableCell className="text-center">{player.nickname}</TableCell>
                 <TableCell className="text-center">{player.level}</TableCell> 
-                <TableCell className="text-center">{player.experience}</TableCell>
-                <TableCell className="text-center">{player.gold}</TableCell>
+                <TableCell className="text-center">{player.experience} xp</TableCell>
+                <TableCell className="text-center">{player.gold} coins</TableCell>
                 <TableCell className="text-center">{player.attributes.constitution + player.attributes.dexterity - player.attributes.insanity / 2 > 0 
-                  ? <div className="flex justify-center"><Image src="/images/icons/heart.png" alt="Heart image" width={64} height={64} className="sepia rounded-full" /></div>
-                  : <div className="flex justify-center"><Image src="/images/icons/cross.png" alt="Cross imager" width={64} height={64} className="sepia rounded-full" /></div>}
+                  ? <div className="flex justify-center"><Tooltip className="w-96 text-4xl mb-4 border-1 rounded-lg border-sepia bg-black/90" size='sm' showArrow={true} content="The Player Is Alive"><Image src="/images/icons/heart.png" alt="Heart image" width={64} height={64} className="sepia rounded-full" /></Tooltip></div>
+                  : <div className="flex justify-center"><Tooltip className="w-96 text-4xl mb-4 border-1 rounded-lg border-sepia bg-black/90" size='sm' showArrow={true} content="The Player Died In The Battle"><Image src="/images/icons/cross.png" alt="Cross imager" width={64} height={64} className="sepia rounded-full" /></Tooltip></div>}
                 </TableCell>
                 <TableCell className="text-center">{player.is_active 
-                  ? <div className="flex justify-center"><Image src="/images/icons/playing.png" alt="Playing image" width={64} height={64} className="sepia rounded-full" /></div>
-                  : <div className="flex justify-center"><Image src="/images/icons/legend.png" alt="Legend image" width={64} height={64} className="sepia rounded-full" /></div>}
+                  ? <div className="flex justify-center"><Tooltip className="w-96 text-4xl mb-4 border-1 rounded-lg border-sepia bg-black/90" size='sm' showArrow={true} content="Currently Suffering"><Image src="/images/icons/playing.png" alt="Playing image" width={64} height={64} className="sepia rounded-full" /></Tooltip></div>
+                  : <div className="flex justify-center"><Tooltip className="w-96 text-4xl mb-4 border-1 rounded-lg border-sepia bg-black/90" size='sm' showArrow={true} content="The Player is Legend"><Image src="/images/icons/legend.png" alt="Legend image" width={64} height={64} className="sepia rounded-full" /></Tooltip></div>}
                 </TableCell>
               </TableRow>
             ))}
