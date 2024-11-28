411986033da910f5d4ec3f10ba8f1f29d1496425
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-28 12:16:00 +0100

    [ER6-267][ADD] Icon in first div

--- src/components/shop/LeftMainContainer.tsx
+++ src/components/shop/LeftMainContainer.tsx
@@ -2,8 +2,18 @@ const LeftMainContainer = () => {
 	return (
 		<div className="flex flex-col justify-start items-center w-3/12 bg-black/85 p-4 rounded-md ml-1">
 
-			<div className="flex flex-col justify-center items-center w-6/12 border-2 border-white p-4 rounded-md text-white">
-				Shopping Icons
+			<div className="flex flex-col justify-center items-center w-6/12 border-2 border-white rounded-md text-white">
+				<div className="flex justify-around w-full">
+					<button className="w-10 h-10 bg-gray-300 rounded-full flex justify-center items-center hover:bg-gray-400 transition">
+						<img src="https://via.placeholder.com/32" alt="Icon 1" className="w-8 h-8" />
+					</button>
+					<button className="w-10 h-10 bg-gray-300 rounded-full flex justify-center items-center hover:bg-gray-400 transition">
+						<img src="https://via.placeholder.com/32" alt="Icon 2" className="w-8 h-8" />
+					</button>
+					<button className="w-10 h-10 bg-gray-300 rounded-full flex justify-center items-center hover:bg-gray-400 transition">
+						<img src="https://via.placeholder.com/32" alt="Icon 3" className="w-8 h-8" />
+					</button>
+				</div>
 			</div>
 
 			<div className="flex flex-col justify-center items-center w-8/12 border-2 border-white rounded-md text-white mt-4 h-80">
@@ -25,6 +35,7 @@ const LeftMainContainer = () => {
 			<div className="flex flex-col justify-center items-center w-10/12 border-2 border-white rounded-md text-white h-96">
 				Object Type Buttons
 			</div>
+
 		</div>
 	);
 };
