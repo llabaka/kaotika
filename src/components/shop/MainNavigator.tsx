2c5fb0a8bd1540c62bf9ddbf92f20a8076c90771
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-11-28 13:27:22 +0100

    [ER6-268][DELETE] Hover color deleted for buttons

--- src/components/shop/MainNavigator.tsx
+++ src/components/shop/MainNavigator.tsx
@@ -30,17 +30,17 @@ const MainNavigator = () => {
         <div className="justify-center flex items-center text-center h-full relative z-1">
                 <button
                     onClick={() => handleButtonClick(0)} // Pasa el índice 0 al hacer clic
-                    className={`flex-1 h-5/6 w-2/3 font-semibold rounded-lg transition-all duration-100 group ${selected === 0 ? 'text-orange-400 text-5xl' : 'text-white text-4xl'} hover:bg-gray-400`}
+                    className={`flex-1 h-5/6 w-2/3 font-semibold rounded-lg transition-all duration-100 group ${selected === 0 ? 'text-orange-400 text-5xl' : 'text-white text-4xl'}`}
                 >
-                <p className="text-center group-hover:text-orange-300 transition-all duration-100">EQUIPMENT</p>
+                <p className="text-center group-hover:text-orange-200 transition-all duration-100">EQUIPMENT</p>
 
             </button>
 
             <button
                 onClick={() => handleButtonClick(1)} // Pasa el índice 1 al hacer clic
-                className={`flex-1 h-5/6 w-2/3 font-semibold rounded-lg transition-all duration-100 group ${selected === 1 ? 'text-orange-400 text-5xl' : 'text-white text-4xl'} hover:bg-gray-400`}  
+                className={`flex-1 h-5/6 w-2/3 font-semibold rounded-lg transition-all duration-100 group ${selected === 1 ? 'text-orange-400 text-5xl' : 'text-white text-4xl'}`}  
                 >
-                <p className="text-center group-hover:text-orange-300 transition-all duration-100   ">MAGIC STUFF</p>
+                <p className="text-center group-hover:text-orange-200 transition-all duration-100   ">MAGIC STUFF</p>
 
 
             </button>
