a6f7555a3b06786424457660e210b55a014aa751
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-28 12:05:32 +0100

    [ER6-267][ADD] Left Containers main divs

--- src/components/shop/LeftMainContainer.tsx
+++ src/components/shop/LeftMainContainer.tsx
@@ -1,10 +1,32 @@
-
 const LeftMainContainer = () => {
 	return (
-		<div className="flex flex-col justify-between items-center w-3/12 bg-black/85 p-4 rounded-md ml-1">
-		Left Container
-	  </div>
-	);
-  };
+		<div className="flex flex-col justify-start items-center w-3/12 bg-black/85 p-4 rounded-md ml-1">
 
-  export default LeftMainContainer;
+			<div className="flex flex-col justify-center items-center w-6/12 border-2 border-white p-4 rounded-md text-white">
+				Shopping Icons
+			</div>
+
+			<div className="flex flex-col justify-center items-center w-8/12 border-2 border-white rounded-md text-white mt-4 h-80">
+				Seller Image
+			</div>
+
+			<div className="flex flex-col justify-center items-center w-11/12 border-2 border-white rounded-md text-white">
+				Separator Image
+			</div>
+
+			<div className="flex flex-col justify-center items-center w-10/12 border-2 border-white rounded-md text-white h-32">
+				Player Info Buttons
+			</div>
+
+			<div className="flex flex-col justify-center items-center w-11/12 border-2 border-white rounded-md text-white">
+				Separator Image2
+			</div>
+
+			<div className="flex flex-col justify-center items-center w-10/12 border-2 border-white rounded-md text-white h-96">
+				Object Type Buttons
+			</div>
+		</div>
+	);
+};
+
+export default LeftMainContainer;
