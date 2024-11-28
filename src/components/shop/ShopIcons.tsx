8648d8bade5ee921e4b421166912f87c40b583ec
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-28 13:20:38 +0100

    [ER6-267][ADD] Scale when pressing button

--- src/components/shop/ShopIcons.tsx
+++ src/components/shop/ShopIcons.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const ShopIcons = () =>{
+
+    const [isPressed, setIsPressed] = useState(false);
+
+    const handleOnPress = () =>  {
+		console.log("Boton pulsado");
+        setIsPressed(prev => !prev);
+	}
 
     return (
         <div className="flex justify-around w-full">
-            <button className="w-10 h-10 rounded-full flex justify-center items-center">
-                <i className='bx bxs-purchase-tag-alt text-2xl hover:text-orange-400 transition'></i>
+            <button onClick={handleOnPress} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className={`bx bxs-purchase-tag-alt text-2xl transition transform ${isPressed ? 'scale-125 text-orange-400' : ''} hover:text-orange-400`}></i>
             </button>
             <button className="w-10 h-10 rounded-full flex justify-center items-center">
                 <i className='bx bx-dollar-circle text-2xl hover:text-orange-400 transition'></i>
