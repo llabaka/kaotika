ccb9b79d87b3ef25fd0f8d8193013e3522c429ea
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-28 13:06:03 +0100

    [ER6-267][UPDATE] Icons style when hover

--- src/components/shop/ShopIcons.tsx
+++ src/components/shop/ShopIcons.tsx
@@ -2,14 +2,14 @@ const ShopIcons = () =>{
 
     return (
         <div className="flex justify-around w-full">
-            <button className="w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-400 transition">
-                <i className='bx bxs-purchase-tag-alt text-2xl'></i>
+            <button className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className='bx bxs-purchase-tag-alt text-2xl hover:text-orange-400 transition'></i>
             </button>
-            <button className="w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-400 transition">
-                <i className='bx bx-dollar-circle text-2xl'></i>
+            <button className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className='bx bx-dollar-circle text-2xl hover:text-orange-400 transition'></i>
             </button>
-            <button className="w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-400 transition">
-                <i className="bx bxs-cart text-2xl"></i>
+            <button className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className="bx bxs-cart text-2xl hover:text-orange-400 transition"></i>
             </button>
         </div>
     )
