b4d18f9ac4fa25402f47cc9ddc3c682a44870003
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-11-28 13:43:16 +0100

    [ER6-269][ADD] MainSeparator image

--- src/components/shop/MiddleMainContainer.tsx
+++ src/components/shop/MiddleMainContainer.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
 const MiddleMainContainer = () => {
 	return (
-        <div className="flex flex-col justify-between items-center w-2 bg-white/85 p-4 rounded-md">
-		    Mid Container
-	    </div>
+		     <div className="relative w-12 h-full">
+
+				<Image
+				src="/images/shop/MainSeparator.png"
+				alt="MainSeparator"
+				layout="fill"
+				className="rounded-lg z-1"
+				/>
+
+
+			</div>
 	);
 };
 
