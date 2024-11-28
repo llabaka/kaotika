4aa0e868b2cfe18c35b77e1f2a1edae26723a558
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-28 13:38:15 +0100

    [ER6-267][UPDATE] Buttons color and scale

--- src/components/shop/ShopIcons.tsx
+++ src/components/shop/ShopIcons.tsx
@@ -2,23 +2,38 @@ import { useState } from "react";
 
 const ShopIcons = () =>{
 
-    const [isPressed, setIsPressed] = useState(false);
+    const [isTicketPressed, setIsTicketPressed] = useState(true);
+    const [isDollarPressed, setIsDollarPressed] = useState(false);
+    const [isCartPressed, setIsCartPressed] = useState(false);
 
-    const handleOnPress = () =>  {
-		console.log("Boton pulsado");
-        setIsPressed(prev => !prev);
+    const handleOnPressTicket = () =>  {
+        setIsTicketPressed(prev => !prev);
+        setIsDollarPressed(false);
+        setIsCartPressed(false);
+	}
+
+    const handleOnPressDollar = () =>  {
+        setIsDollarPressed(prev => !prev);
+        setIsTicketPressed(false);
+        setIsCartPressed(false);
+	}
+
+    const handleOnPressCart = () =>  {
+        setIsCartPressed(prev => !prev);
+        setIsTicketPressed(false);
+        setIsDollarPressed(false);
 	}
 
     return (
         <div className="flex justify-around w-full">
-            <button onClick={handleOnPress} className="w-10 h-10 rounded-full flex justify-center items-center">
-                <i className={`bx bxs-purchase-tag-alt text-2xl transition transform ${isPressed ? 'scale-125 text-orange-400' : ''} hover:text-orange-400`}></i>
+            <button onClick={handleOnPressTicket} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className={`bx bxs-purchase-tag-alt text-2xl transition transform ${isTicketPressed ? 'scale-125 text-orange-400' : 'scale-80 text-gray-400'} hover:text-orange-400`}></i>
             </button>
-            <button className="w-10 h-10 rounded-full flex justify-center items-center">
-                <i className='bx bx-dollar-circle text-2xl hover:text-orange-400 transition'></i>
+            <button onClick={handleOnPressDollar} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className={`bx bx-dollar-circle text-2xl transition transform ${isDollarPressed ? 'scale-125 text-orange-400' : 'scale-80 text-gray-400'} hover:text-orange-400`}></i>
             </button>
-            <button className="w-10 h-10 rounded-full flex justify-center items-center">
-                <i className="bx bxs-cart text-2xl hover:text-orange-400 transition"></i>
+            <button onClick={handleOnPressCart} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <i className={`bx bxs-cart text-2xl transition transform ${isCartPressed ? 'scale-125 text-orange-400' : 'scale-80 text-gray-400'} hover:text-orange-400`}></i>
             </button>
         </div>
     )
