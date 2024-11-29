0d8eb2219b955f01c71ea9bdd030bf9eb1c44e24
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-29 09:37:05 +0100

    [ER6-270][ADD] Text to buttons

--- src/components/shop/LeftMainContainer.tsx
+++ src/components/shop/LeftMainContainer.tsx
@@ -21,30 +21,33 @@ const LeftMainContainer = () => {
 			<div className="flex flex-col justify-center items-center w-10/12 border-2 border-white rounded-md text-white h-32">
 				<div className="flex justify-around">
 					{/* First Image */}	
-					<div className="w-40 h-12 flex relative">
+					<div className="w-40 h-12 flex relative items-center justify-center">
 						<Image
 							src="/images/shop/ManagePlayerButton.png"
 							alt="Imagen 1"
 							layout="fill"
 						/>
+						<span className="z-10 text-orange-400 text-2xl">1230€</span>
 					</div>
 					{/* Second Image */}
-					<div className="w-40 h-12 flex relative">
+					<div className="w-40 h-12 flex relative items-center justify-center">
 						<Image
 							src="/images/shop/ManagePlayerButton.png"
 							alt="Imagen 1"
 							layout="fill"
 						/>
+						<span className="z-10 text-orange-400 text-2xl">LVL 17</span>
 					</div>
 				</div>
 
 					{/* Third Image */}
-					<div className="w-60 h-16 flex relative">
+					<div className="w-60 h-16 flex relative items-center justify-center">
 						<Image
 							src="/images/shop/ManagePlayerButton.png"
 							alt="Imagen 1"
 							layout="fill"
 						/>
+						<span className="z-10 text-orange-400 text-2xl">MANAGE PLAYER</span>
 					</div>
 			</div>
 
