a8ebcc1e932d4e6dd566ae86b41c8f77caa83894
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-11-29 13:12:43 +0100

    [ER6-271][UPDATE] Card separator added after every card item

--- src/components/shop/ProductRowContainer.tsx
+++ src/components/shop/ProductRowContainer.tsx
@@ -1,5 +1,6 @@
 import { CardProps } from "@/_common/interfaces/shop/CardProps";
 import CardItem from "./CardItem";
+import React from "react";
 
 interface ProductRowContainerProps {
   cards: CardProps[]; // Espera un array de objetos con la interfaz CardProps
@@ -9,7 +10,13 @@ const ProductRowContainer: React.FC<ProductRowContainerProps> = ({ cards }) => {
   return (
     <div className="flex w-full h-[49%] bg-gray-500 justify-center items-center text-center">
       {cards.map((card, index) => (
-        <CardItem key={index} card={card} />
+        <React.Fragment key={index}>
+          <CardItem card={card} />
+          {/* Mostrar el separador solo si no es el último item */}
+          {index < cards.length - 1 && (
+            <div className="flex w-[2%] h-full bg-orange-500 text-white"></div>
+          )}
+        </React.Fragment>
       ))}
     </div>
   );
