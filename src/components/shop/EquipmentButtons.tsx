1fe5ccac76d1c6cd8faf9b72157e0d7b9248f9fe
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-29 12:44:55 +0100

    [ER6-270][UPDATE] Scrollbar style

--- src/components/shop/EquipmentButtons.tsx
+++ src/components/shop/EquipmentButtons.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const EquipmentButtons = () => {
     return (
-        <div className="flex-col justify-start items-center w-10/12 h-80 to overflow-y-scroll mt-1">
+        <div className="flex-col justify-start items-center w-10/12 h-80 mt-1 max-h-[400px] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-orange-100 [&::-webkit-scrollbar-thumb]:bg-orange-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-orange-400 pr-2">
             <button className="w-full h-16 flex relative items-center justify-center mb-4">
                 <Image
                     src="/images/shop/ManagePlayerButton.png"
