46b8ee861134087d7bce5c62fcbd0d6d2c59d695
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-29 09:58:02 +0100

    [ER6-270][ADD] Player Stats buttons component

--- src/components/shop/LeftMainContainer.tsx
+++ src/components/shop/LeftMainContainer.tsx
@@ -1,6 +1,7 @@
 import 'boxicons/css/boxicons.min.css';
 import ShopIcons from './ShopIcons';
 import Image from 'next/image';
+import PlayerStatsButtons from './PlayerStatsButtons';
 
 const LeftMainContainer = () => {
 	return (
@@ -18,37 +19,8 @@ const LeftMainContainer = () => {
 				Separator Image
 			</div>
 
-			<div className="flex flex-col justify-center items-center w-10/12 border-2 border-white rounded-md text-white h-32">
-				<div className="flex justify-around">
-					{/* First Image */}	
-					<div className="w-40 h-12 flex relative items-center justify-center">
-						<Image
-							src="/images/shop/ManagePlayerButton.png"
-							alt="Imagen 1"
-							layout="fill"
-						/>
-						<span className="z-10 text-orange-400 text-2xl">1230€</span>
-					</div>
-					{/* Second Image */}
-					<div className="w-40 h-12 flex relative items-center justify-center">
-						<Image
-							src="/images/shop/ManagePlayerButton.png"
-							alt="Imagen 1"
-							layout="fill"
-						/>
-						<span className="z-10 text-orange-400 text-2xl">LVL 17</span>
-					</div>
-				</div>
-					{/* Third Imagen Button */}
-					<button className="w-60 h-16 flex relative items-center justify-center ">
-						<Image
-							src="/images/shop/ManagePlayerButton.png"
-							alt="Imagen 1"
-							layout="fill"
-						/>
-						<span className="z-10 text-orange-400 text-2xl hover:text-orange-200 transition">MANAGE PLAYER</span>
-					</button>
-			</div>
+			{/* Player Stats Buttons */}
+			<PlayerStatsButtons></PlayerStatsButtons>
 
 			<div className="flex flex-col justify-center items-center w-11/12 border-2 border-white rounded-md text-white">
 				Separator Image2
