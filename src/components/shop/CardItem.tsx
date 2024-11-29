19881f2a28c584ea1d951feab392dcb504eeb0cf
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-11-29 13:25:13 +0100

    [ER6-271][UPDATE] Card rendering by props

--- src/components/shop/CardItem.tsx
+++ src/components/shop/CardItem.tsx
@@ -5,26 +5,6 @@ interface CardItemProps {
     card: CardProps;
   }
 
-const cardMock : CardProps = {
-    minLevel: 12,
-    image: '/images/equipment/armors/jacket_1.png',
-    modifiers: {
-        intelligence: 12,
-        dexterity: 12,
-        constitution: 20,
-        insanity: 43,
-        charisma: 23,
-        strength: 34
-    },
-    name: 'cardMock',
-    description: 'Description prubea de carta lkasndjoasbdiuasd',
-    icon: '/images/icons/up.png',
-    value: 340, 
-    defense: 32
-};
-
-const cards=[Card(cardMock), Card(cardMock), Card(cardMock)]
-
 const CardItem: React.FC<CardItemProps> = ({ card }) => {
     return(
         <div className="flex w-[31.5%] bg-blue-500 text-center items-center justify-center text-white hover:scale-105 transition">
