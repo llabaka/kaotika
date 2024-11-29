cd520d70a5eef72174c36195a68fb5f30bba6a1e
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-29 09:04:15 +0100

    [ER6-270][ADD] Button image in container

--- src/components/shop/LeftMainContainer.tsx
+++ src/components/shop/LeftMainContainer.tsx
@@ -1,5 +1,6 @@
 import 'boxicons/css/boxicons.min.css';
 import ShopIcons from './ShopIcons';
+import Image from 'next/image';
 
 const LeftMainContainer = () => {
 	return (
@@ -18,7 +19,20 @@ const LeftMainContainer = () => {
 			</div>
 
 			<div className="flex flex-col justify-center items-center w-10/12 border-2 border-white rounded-md text-white h-32">
-				Player Info Buttons
+				<div className="flex justify-around">
+					{/* First Image */}	
+					<div className="w-40 h-12 flex relative">
+						<Image
+							src="/images/shop/ManagePlayerButton.png"
+							alt="Imagen 1"
+							layout="fill"
+						/>
+					</div>
+					{/* Second Image */}
+					<img src="https://via.placeholder.com/50" alt="Imagen 2" className="w-12 h-12 rounded-md"/>
+				</div>
+
+				<img src="https://via.placeholder.com/50" alt="Imagen 3" className="w-12 h-12 rounded-md"/>
 			</div>
 
 			<div className="flex flex-col justify-center items-center w-11/12 border-2 border-white rounded-md text-white">
