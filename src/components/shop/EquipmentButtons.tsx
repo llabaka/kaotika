2fc5ea756bd26a0c7593e330b537d955aebac6ec
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-02 09:33:38 +0100

    [ER6-270][UPDATE] Hover logic. Button scales when hovering

--- src/components/shop/EquipmentButtons.tsx
+++ src/components/shop/EquipmentButtons.tsx
@@ -22,23 +22,23 @@ const EquipmentButtons = () => {
         <div className="flex-col justify-start items-center w-10/12 h-80 mt-1 max-h-[400px] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-orange-100 [&::-webkit-scrollbar-thumb]:bg-orange-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-orange-400 pr-2">
         {buttons.map(({ id, label }) => (
             <button
-            key={id}
-            onClick={() => handleButtonClick(id)}
-            className="w-full h-16 flex relative items-center justify-center mb-4"
+                key={id}
+                onClick={() => handleButtonClick(id)}
+                className={`w-[98%] h-16 flex relative items-center justify-center mb-4 transform transition-transform ${activeButton !== id ? 'hover:scale-105' : ''}`}
             >
-            <Image
-                src="/images/shop/ManagePlayerButton.png"
-                alt={label}
-                layout="fill"
-                className={`transform transition ${activeButton === id ? 'scale-100' : 'scale-90'}`}
-            />
-            <span
-                className={`z-10 text-orange-400 hover:text-orange-200 transition-all ${
-                activeButton === id ? 'text-3xl text-orange-200' : 'text-2xl'
-                }`}
-            >
-                {label}
-            </span>
+                <Image
+                    src="/images/shop/ManagePlayerButton.png"
+                    alt={label}
+                    layout="fill"
+                    className={`transform transition ${activeButton === id ? 'scale-100' : 'scale-90'}`}
+                />
+                <span
+                    className={`z-10 text-orange-400 transition-all ${
+                        activeButton === id ? 'text-3xl text-orange-300' : 'text-2xl'
+                    } hover:text-orange-200`}
+                >
+                    {label}
+                </span>
             </button>
         ))}
         </div>
