e6acebfac82c6afd010f851ac72b313ff925096f
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-12-02 07:52:39 +0100

    [ER6-271][ADD] Empty Card component

---
+++ src/components/shop/Card/EmptyCard.tsx
@@ -0,0 +1,12 @@
+import { CardProps } from "@/_common/interfaces/shop/CardProps";
+
+const EmptyCard = (props: CardProps) => {
+
+    return(
+            <div className="w-[25rem] relative h-[23rem] flex flex-row">
+                
+            </div>
+    )
+};
+
+export default EmptyCard;
