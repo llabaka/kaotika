6643011f440bb21cd37a71722a1bcf5acb4936a5
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-03 10:58:49 +0100

    [ER6-277][ADD] Title to the icons

--- src/components/shop/ShopIcons.tsx
+++ src/components/shop/ShopIcons.tsx
@@ -27,13 +27,13 @@ const ShopIcons = () =>{
     return (
         <div className="flex flex-col justify-center items-center w-6/12 rounded-md">
             <div className="flex justify-around w-full">
-                <button onClick={handleOnPressTicket} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <button onClick={handleOnPressTicket} title='Shop' className="w-10 h-10 rounded-full flex justify-center items-center">
                     <i className={`bx bxs-purchase-tag-alt text-2xl transition transform ${isTicketPressed ? 'scale-150 text-orange-400' : ' text-gray-400'} hover:text-orange-400`}></i>
                 </button>
-                <button onClick={handleOnPressDollar} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <button onClick={handleOnPressDollar} title='Sell' className="w-10 h-10 rounded-full flex justify-center items-center">
                     <i className={`bx bx-dollar-circle text-2xl transition transform ${isDollarPressed ? 'scale-150 text-orange-400' : ' text-gray-400'} hover:text-orange-400`}></i>
                 </button>
-                <button onClick={handleOnPressCart} className="w-10 h-10 rounded-full flex justify-center items-center">
+                <button onClick={handleOnPressCart} title='Cart' className="w-10 h-10 rounded-full flex justify-center items-center">
                     <i className={`bx bxs-cart text-2xl transition transform ${isCartPressed ? 'scale-150 text-orange-400' : ' text-gray-400'} hover:text-orange-400`}></i>
                 </button>
             </div>
