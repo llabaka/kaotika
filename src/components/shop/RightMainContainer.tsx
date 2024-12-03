7231f04f71ecb056d189fa767b76d70334eb68a1
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-12-03 11:43:28 +0100

    [ER6-266][FIX]RightMainContainer

--- src/components/shop/RightMainContainer.tsx
+++ src/components/shop/RightMainContainer.tsx
@@ -4,13 +4,19 @@ import { CardProps } from "@/_common/interfaces/shop/CardProps";
 import MainNavigator from "./MainNavigator";
 
 import ProductsContainer from "./ProductsContainer";
+import React from "react";
 
-const RightMainContainer = () => {
+interface RightContainerInterface {
+	products: CardProps[];
+	displayingScreen: Number;
+}
+
+const RightMainContainer:React.FC<RightContainerInterface> = ({products, displayingScreen}) => {
 
 	return (
         <div className="flex flex-col justify-start items-center w-9/12 bg-transparent p-1 rounded-md mr-2">
 			<MainNavigator/>
-			<ProductsContainer/>
+			<ProductsContainer products={products}/>
 		</div>
 	);
 };
