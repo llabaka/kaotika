b8ca0be920f60d60130757fd260b31f422712fca
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-12-04 11:35:57 +0100

    [ER6-279][ADD] Cart product removing button

--- src/components/shop/CartItems.tsx
+++ src/components/shop/CartItems.tsx
@@ -2,12 +2,15 @@ import CartInterface from "@/_common/interfaces/shop/CartInterface";
 import React, { useState } from "react";
 import Image from "next/image";
 import { CardProps } from "@nextui-org/react";
+import { Product } from "@/_common/interfaces/shop/CardProps";
 
 const CartItems:React.FC<CartInterface> = ({cartProducts, setCartProducts}) => {
 
     // const [products, setProducts] = useState<CardProps[] | []>(cartProducts);
 
-
+    const handleRemoveItem = (id: string) => {
+        setCartProducts((prevItems:Product[]) => prevItems.filter((item:Product) => item._id !== id));
+      };
 
     return(
         <div className="h-[85%] w-[90%] text-xl border-white border-2 overflow-y-scroll">
@@ -23,6 +26,14 @@ const CartItems:React.FC<CartInterface> = ({cartProducts, setCartProducts}) => {
 
                         {/* Cantidad del objeto */}
                         <div className=" text-4xl text-orange-300 ml-auto mr-5">{item.quantity}</div>
+
+                         {/* Botón para eliminar elemento */}
+                        <button
+                            onClick={() => handleRemoveItem(item._id!)}
+                            className="ml-4 px-3 py-1 bg-red-600 text-white text-xl rounded hover:bg-red-700"
+                        >
+                            Remove Item
+                        </button>
 
                         {/* Valor del objeto */}
                         <div className=" text-5xl text-orange-300 ml-auto mr-5">{item.value}</div>
