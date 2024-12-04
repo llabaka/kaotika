7307b3931b1bd1a5223f3b6915d94d8734ef05a5
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-04 10:18:20 +0100

    [ER6-278][UPDATE] Tabulation in SellingItem component

--- src/components/shop/SellingItem.tsx
+++ src/components/shop/SellingItem.tsx
@@ -7,19 +7,19 @@ interface SellingItemInterface{
 const SellingItem: React.FC<SellingItemInterface> = ({ sellingImage }) => {
     return (
         <div className="w-full h-[75%] items-center justify-center">
-        <div className="flex relative w-full h-full items-center justify-center">
-            <Image
-                src="/images/shop/SellingFrame.png"
-                alt="Selling Item Frame"
-                fill
-                sizes='(max-width: 426px) 100vw'
-                />
+            <div className="flex relative w-full h-full items-center justify-center">
+                <Image
+                    src="/images/shop/SellingFrame.png"
+                    alt="Selling Item Frame"
+                    fill
+                    sizes='(max-width: 426px) 100vw'
+                    />
 
-            <div className="flex w-[95%] h-[95%] items-center justify-center">
-                <img src={`${sellingImage}`} alt="Selling Image" sizes='(max-width: 780px) 100vw' className="z-10 rounded-lg"/>
+                <div className="flex w-[95%] h-[95%] items-center justify-center">
+                    <img src={`${sellingImage}`} alt="Selling Image" sizes='(max-width: 780px) 100vw' className="z-10 rounded-lg"/>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
