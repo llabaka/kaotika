26dbe659fa984d73b7e1caa296eeaec2dcd5f061
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-04 08:33:14 +0100

    [ER6-278][ADD] Coin image and div to items image

--- src/components/shop/InventoryContainer.tsx
+++ src/components/shop/InventoryContainer.tsx
@@ -1,4 +1,5 @@
 import { Player } from "@/_common/interfaces/Player";
+import Image from 'next/image';
 
 interface InventoryContainer {
     player: Player;
@@ -26,14 +27,29 @@ const InventoryContainer: React.FC<InventoryContainer> = ({ player }) => {
             <div className="flex flex-col justify-start items-center h-[85%] mt-[10%] overflow-y-auto max-h-[full] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-orange-100 [&::-webkit-scrollbar-thumb]:bg-orange-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-orange-400 pr-2">
                 {playerInventory.map((item, index) => (
                     <div key={index} className="flex flex-start items-center bg-black bg-opacity-50 rounded-lg w-[95%] h-[23%] mb-[4%]">
-                        {/* Imagen del objeto */}
-                        <img src={item.image} alt={item.name} className="w-[15%] h-[50%] object-cover m-[5%]" />
+                        
+                        {/* Item image */}
+                        <div className="w-[15%] h-[60%] object-cover m-[5%]">
+                            <img src={item.image} alt={item.name} className="w-[full] h-[full]"></img>
+                        </div>
 
-                        {/* Nombre del objeto */}
-                        <div className="text-3xl text-orange-300">{item.name}</div>
+                        {/* Item name */}
+                        <div className="text-4xl text-white">{item.name}</div>
 
-                        {/* Valor del objeto */}
-                        <div className=" text-5xl text-orange-300 ml-auto mr-[5%]">{item.value}</div>
+                        {/* Item value */}
+                        <div className=" text-5xl text-orange-300 ml-auto mr-[3%]">{item.value}</div>
+
+                        {/* Coin Image */}
+                        <div className="justify-center">
+                            <Image
+                            src="/images/shop/CoinsIcon.png"
+                            alt="Gold"
+                            width={24}
+                            height={24}
+                            className="mt-3 mr-2"
+                            style={{height: 'auto', width:'auto'}}
+                        />
+                    </div>
                     </div>
                 ))}
             </div>
