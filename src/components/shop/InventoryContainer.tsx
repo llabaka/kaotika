06b4ead35897f32b1b02af9ee40ac45f3aea38d4
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-04 08:12:21 +0100

    [ER6-278][UPDATE] Scrollbar style

--- src/components/shop/InventoryContainer.tsx
+++ src/components/shop/InventoryContainer.tsx
@@ -23,7 +23,7 @@ const InventoryContainer: React.FC<InventoryContainer> = ({ player }) => {
     console.log(playerInventory); 
     return (
         <div className="flex-col w-[47%]">
-            <div className="flex flex-col justify-start items-center h-[80%] mt-10 overflow-y-auto max-h-[full]">
+            <div className="flex flex-col justify-start items-center h-[85%] mt-[10%] overflow-y-auto max-h-[full] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-orange-100 [&::-webkit-scrollbar-thumb]:bg-orange-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-orange-400 pr-2">
                 {playerInventory.map((item, index) => (
                     <div key={index} className="flex flex-start items-center bg-black bg-opacity-50 rounded-lg w-[95%] h-[23%] mb-4">
                         {/* Imagen del objeto */}
