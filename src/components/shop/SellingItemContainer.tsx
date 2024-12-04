e55cce550f85f9cb34517cabd5f4a698037d9e23
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-04 09:03:52 +0100

    [ER6-278][ADD] Selling Item frame

--- src/components/shop/SellingItemContainer.tsx
+++ src/components/shop/SellingItemContainer.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+
 const SellingItemContainer = () =>  {
     return (
         <div className="flex-col w-[47%]">
-            <div className="flex flex-col justify-start items-center border-2 h-[90%] mt-10 border-white">
-                <div className="border-2 w-[95%] h-[23%] border-white"></div>
+            <div className="flex flex-col justify-start items-center h-[97%] mt-[5%]">
+                <div className="w-[95%] h-[75%] items-center justify-center">
+                    <div className="flex relative w-full h-full">
+                        <Image
+                            src="/images/shop/SellingFrame.png"
+                            alt="Selling Item Frame"
+                            fill
+                            sizes='(max-width: 426px) 100vw'
+                            />
+                    </div>
+                </div>
             </div>
         </div>
     )
