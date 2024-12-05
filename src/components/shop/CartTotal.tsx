2a15b3ee4bd881903813a8623e4264aeceec2c88
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-12-05 09:49:06 +0100

    [ER6-279][UPDATE]CartTotal style

--- src/components/shop/CartTotal.tsx
+++ src/components/shop/CartTotal.tsx
@@ -7,20 +7,22 @@ import Image from "next/image";
 const CartTotal:React.FC<CartPriceInterface> = ({totalPrice}) => {
 
     return(
-        <div className="h-[13%] w-[90%] text-xl">
+        <div className="h-[13%] w-[90%] text-xl bg-black bg-opacity-50 rounded-md">
             <div className="flex h-[100%] w-[100%] text-xl justify-between items-center">
                 <p className="h-[80%] ml-3 text-3xl">TOTAL<span className="text-5xl"> :</span></p>
                 <div className="flex w-[25%] h-[80%] justify-center">
-                    <div className="w-[90%] mr-3 text-5xl text-right">{totalPrice}</div>
-                    <div className="w-[20%] flex items-center justify-center z-1 text-orange-400 text-3xl mr-6">
-                            <Image
-                            src="/images/shop/CoinsIcon.png"
-                            alt="Gold"
-                            width={24}
-                            height={24}
-                            className="ml-2"
-                            style={{height: 'auto', width:'auto'}}
-                            />
+
+                    <p className="w-[70%] h-full text-5xl text-right mr-2" >{totalPrice}</p>
+
+                    <div className="w-[20%] h-[80%]">
+                        <div className="relative w-full h-full">
+                                <Image
+                                src="/images/shop/CoinsIcon.png"
+                                alt="Gold"
+                                fill
+                                sizes='(max-width: 50px) 100vw'
+                                />
+                            </div>
                         </div>
                 </div>
                
