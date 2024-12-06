d596b63da6fc55450300249beb68f44576319884
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-06 11:37:12 +0100

    [ER6-249][UPDATE] Left Container interfaces. Real player is used

--- src/components/shop/LeftMainContainer.tsx
+++ src/components/shop/LeftMainContainer.tsx
@@ -6,41 +6,40 @@ import ButtonsSeparator from './ButtonsSeparator';
 import EquipmentButtons from './EquipmentButtons';
 import SellerSeparator from './SellerSeparator';
 import Seller from './Seller';
-import player from '../../data/player.json'
 import { Product } from '@/_common/interfaces/shop/Product';
 import { AllProducts } from '@/_common/interfaces/shop/AllProducts';
+import { Player } from '@/_common/interfaces/Player';
 
 interface LeftMainContainerInterface {
     setDisplayingScreen: (loaded:number) => void;
 	allProducts: AllProducts;
 	showingProducts: Product[];
+	player: Player
 	setShowingProducts: (loaded: Product[]) => void;
 }
 
-const LeftMainContainer:React.FC<LeftMainContainerInterface> = ({setDisplayingScreen, allProducts, showingProducts, setShowingProducts})=> {
-
-	const mockPlayer = player;
+const LeftMainContainer:React.FC<LeftMainContainerInterface> = ({setDisplayingScreen, allProducts, showingProducts, setShowingProducts, player})=> {
 
 	return (
 		<div className="flex flex-col justify-start items-center w-3/12 p-4 rounded-md">
 
 			{/* Shopping Icons */}
-			<ShopIcons setDisplayingScreen={setDisplayingScreen} />
+			<ShopIcons setDisplayingScreen={setDisplayingScreen}/>
 
 			{/* Seller Frame and Image */}
-			<Seller></Seller>
+			<Seller/>
 
 			{/* Seller Separator */}
-			<SellerSeparator></SellerSeparator>
+			<SellerSeparator/>
 
 			{/* Player Stats Buttons */}
-			<PlayerStatsButtons player={mockPlayer} />
+			<PlayerStatsButtons player={player} />
 
 			{/* Buttons Separator */}
-			<ButtonsSeparator></ButtonsSeparator>
+			<ButtonsSeparator/>
 
 			{/* Equipment Buttons */}
-			<EquipmentButtons allProducts={allProducts} showingProducts={showingProducts} setShowingProducts={setShowingProducts}></EquipmentButtons>
+			<EquipmentButtons allProducts={allProducts} showingProducts={showingProducts} setShowingProducts={setShowingProducts}/>
 		</div>
 	);
 };
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -159,7 +159,7 @@ const Shop = () => {
       <Layout>
         <div className=" text-medievalSepia bg-cover bg-center min-h-screen" style={{ backgroundImage: 'url(/images/map.jpg)'}}>
         <MainShopContainer>
-          <LeftMainContainer setDisplayingScreen={setDisplayingScreen} allProducts={allProducts} showingProducts={showingProducts} setShowingProducts={setShowingProducts}/>
+          <LeftMainContainer setDisplayingScreen={setDisplayingScreen} allProducts={allProducts} showingProducts={showingProducts} setShowingProducts={setShowingProducts} player={player}/>
           <MiddleMainContainer />
           <RightMainContainer 
             products={showingProducts} 
