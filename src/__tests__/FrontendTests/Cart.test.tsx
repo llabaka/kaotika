31710b25094dbd3ef032bc7d29081131438a916f
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-12-08 12:44:31 +0100

    [ER6-287][UPDATE] Separate Frontend and Backend tests

--- src/__tests__/FrontendTests/Cart.test.tsx
+++ src/__tests__/FrontendTests/Cart.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import React from 'react';
 import Cart from '@/components/shop/Cart';
 import { Product } from '@/_common/interfaces/shop/Product';
 
