a4fe5033c770a006a1d7f13b7b95e9a9c1f1bbd6
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-12-10 23:42:03 +0100

    [ER6-303][NEW]TEST FRONTEND - PRODUCT CARD

--- src/components/shop/Card/Components/CardLeftContainer.tsx
+++ src/components/shop/Card/Components/CardLeftContainer.tsx
@@ -16,26 +16,18 @@ interface CardLeftContainerProps {
 }
 
 const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onClickBuy, onClickAddToCart, player} : CardLeftContainerProps) => {
-
-    const [isBought, setIsBought] = useState<boolean>(false);
-    const [isAddToBasket, setIsAddToBasket] = useState<boolean>(false);
-
-    let icon = '/images/icons/up.png';
+    const [isDisableButton, setIsDisableButton] = useState<boolean>(false);
 
     useEffect(() => {
-        const addToBasketButton = document.getElementById(_id);
-
-        if(isAddToBasket){
-            addToBasketButton?.classList.add('opacity-50');
+        if(player.gold < value || player.level < min_lvl!){
+            setIsDisableButton(true);
         }else{
-            addToBasketButton?.classList.remove('opacity-50');
+            setIsDisableButton(false);
         }
         
-    }, [isAddToBasket]);
-    
-    const handleAddToBasket = () => {
-        setIsAddToBasket(prevState => !prevState);
-    }
+    }, [player.gold]);
+
+    let icon = '/images/icons/up.png';
 
     // Change color depending on player's level
     const minLevelClass = player.level >= (min_lvl || 0) ? 'text-green-400' : 'text-red-600';
@@ -44,9 +36,11 @@ const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onCli
             <div className="flex flex-col relative w-[65%] h-[100%] items-center">
             {/* DIV LEVEL */}
             <div className="z-10 relative flex flex-row justify-around items-center w-[100%] h-[16%]">
-                <div className={` text-[35px] font-bold w-[20%] pr-[6%] ${minLevelClass}`}>{min_lvl}</div>
+                <div className={` text-[35px] font-bold w-[20%] pr-[6%] ${minLevelClass}`}
+                    data-testid={`min_lvl_${_id}`}>{min_lvl}</div>
                     <div className="flex justify-around w-[36%] h-[75%] mt-5 items-center text-right">
-                        <p className="text-white w-[65%] text-[25px] font-bold mr-1">{value}</p>
+                        <p className="text-white w-[65%] text-[25px] font-bold mr-1"
+                            data-testid={`product_value_${_id}`}>{value}</p>
                         <div className="w-[35%] h-[55%] mt-1 mr-1">
                             <div className="relative w-full h-full">
                                     <Image
@@ -68,19 +62,21 @@ const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onCli
             </div>
             {/* IMAGE DIV */}
             <div className="flex-col flex justify-center items-center h-[45%] w-[65%] mt-0.5">
-                <div className="h-[100%] w-[100%] flex relative items-center justify-center rounded-lg"> 
+                <div className="h-[100%] w-[100%] flex relative items-center justify-center rounded-lg"
+                    > 
                     <Image 
                         src={image} 
                         alt="Equipment Image"
                         fill
                         sizes='(max-width: 570px) 100vw'
                         className="rounded-lg" // También redondeamos la imagen si es necesario
+                        data-testid={`image_id_${image}`}
                     />
                 </div>
             </div>
             {/* NAME, DESCRIPTION BUTTONS DIV */}
             <div className="h-[10%] text-black text-lg font-bold flex items-center justify-center mt-2">
-                <p>{name}</p>
+                <p data-testid={`product_${name}`} >{name}</p>
             </div>
             <div className="h-[14%] text-[22px] flex content-center justify-center overflow-hidden w-[87%]">
                 <p className="line-clamp-none overflow-y-auto " 
@@ -95,12 +91,16 @@ const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onCli
                 <button className="text-white text-xl ml-5 w-[36%] h-[90%] rounded-full flex items-center justify-center" 
                     onClick={onClickAddToCart}
                     id={_id}
+                    data-testid={`add_basket_${_id}`}
+                    disabled={isDisableButton}
                     >
                         Add Basket
                     </button>
                 <button className="text-white text-xl mr-6 w-[36%] h-[90%] rounded-full flex items-center justify-center " 
                     onClick={onClickBuy}
                     id={`buy_button_${_id}`}
+                    data-testid={`buy_${_id}`}
+                    disabled={isDisableButton}
                     >
                         Buy
                 </button>
