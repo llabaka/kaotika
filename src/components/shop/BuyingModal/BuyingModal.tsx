2f8994b0774f5a946327673a45fd8f46f511310d
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-11 12:50:50 +0100

    [ER6-315][ADD] Not enough gold tooltip message

--- src/components/shop/BuyingModal/BuyingModal.tsx
+++ src/components/shop/BuyingModal/BuyingModal.tsx
@@ -8,7 +8,7 @@ import Loading from "@/components/Loading";
 
 // Open Modal boolean 
 // product
-interface  BuyingModalProps {
+interface BuyingModalProps {
     product: Product | null;
     onclick: () => void;
     player: Player;
@@ -17,7 +17,7 @@ interface  BuyingModalProps {
     setShopTooltips: React.Dispatch<React.SetStateAction<ShopTooltipProps[]>>;
 }
 
-const BuyingModal = ({product, onclick, player, setPlayer, setHaveBuy, setShopTooltips} : BuyingModalProps) => {
+const BuyingModal = ({ product, onclick, player, setPlayer, setHaveBuy, setShopTooltips }: BuyingModalProps) => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const buyingFrame = "/images/shop/BuyingFrameWithBG.png";
@@ -25,27 +25,27 @@ const BuyingModal = ({product, onclick, player, setPlayer, setHaveBuy, setShopTo
     const buttonImage = "/images/shop/ManagePlayerButton.png";
 
     const addToolTip = (image: string, itemName: string, action: string) => {
-        setShopTooltips((prevTooltips : ShopTooltipProps[]) => [...prevTooltips, {image, action, itemName}]);
+        setShopTooltips((prevTooltips: ShopTooltipProps[]) => [...prevTooltips, { image, action, itemName }]);
     }
 
     // Block the scroll
     document.documentElement.style.overflow = "hidden";
-    
-    const buyButtonHandler = async() => {
+
+    const buyButtonHandler = async () => {
 
         // check gold
         setIsLoading(true);
 
         const productValue = product?.value!;
         const playerGold = player.gold;
-        
-        if(playerGold > productValue){
 
-            if(product !== null){
-                const response = await buyProductClient(player._id, [{type: product.type!, productId: product._id!}]);
+        if (playerGold > productValue) {
+
+            if (product !== null) {
+                const response = await buyProductClient(player._id, [{ type: product.type!, productId: product._id! }]);
                 const json = await response.json();
 
-                if(!response.ok){   
+                if (!response.ok) {
                     console.log(JSON.stringify(json));
                     addToolTip("", json.error, "Error");
                     setIsLoading(false);
@@ -58,8 +58,9 @@ const BuyingModal = ({product, onclick, player, setPlayer, setHaveBuy, setShopTo
                 addToolTip(product.image!, product.name!, "bought");
                 console.log("Procede a comprar");
             }
-        }else{
-            console.log("EL jugador no tiene suficiente oro CLIENT");
+        } else {
+            // Show not enough gold message
+            addToolTip(product!.image!, product!.name!, "not enough gold to buy:");
         }
 
         setIsLoading(false);
@@ -68,61 +69,61 @@ const BuyingModal = ({product, onclick, player, setPlayer, setHaveBuy, setShopTo
         document.documentElement.style.overflow = "";
     }
 
-    
+
     return (
-        isLoading ? <Loading/> : 
+        isLoading ? <Loading /> :
 
-        <div className="w-full h-[120%] absolute top-0 z-20 bg-black bg-opacity-70 flex items-center justify-center" id="buy_modal" data-testid="buying-modal">
-            
-            <div className="w-[42%] h-[44%] flex justify-center flex-col px-[2%] py-[2%] relative z-0">
-                <Image 
-                    src={buyingFrame}
-                    fill
-                    alt="Buying frame"
-                    sizes='(max-width: 953px) 100vw'
+            <div className="w-full h-[120%] absolute top-0 z-20 bg-black bg-opacity-70 flex items-center justify-center" id="buy_modal" data-testid="buying-modal">
+
+                <div className="w-[42%] h-[44%] flex justify-center flex-col px-[2%] py-[2%] relative z-0">
+                    <Image
+                        src={buyingFrame}
+                        fill
+                        alt="Buying frame"
+                        sizes='(max-width: 953px) 100vw'
                     />
-                    
-                <p className="text-[34px] text-center z-10 text-white">Are you sure you want to buy the
-                    <span className="text-orange-400"> {product?.name}</span> for <span className="text-orange-400">{product?.value} g</span> ?</p>
-                <div className="w-[95%] h-[68%] flex justify-around items-center ml-[3%]">
-                    {/* DIV PARA IMAGEN Y BOTONES */}
-                    <div className="w-[34%] h-[90%] relative">
-                        <Image 
-                            src={product?.image!}
-                            fill
-                            alt="item_image"
-                            sizes="(max-width: 300px) 100vw"
-                            />
-                            
-                    </div>
-                    <div className="w-[50%] h-[90%] flex flex-col items-center justify-center">
-                        <div className="w-[85%] h-[35%] relative mb-[12%] flex items-center justify-center hover:scale-110 transition-all cursor-pointer"
-                            data-testid={'ConfirmButton'}
-                            onClick={buyButtonHandler}>
-                            <Image
-                                src={buttonImage}
-                                fill
-                                alt="Button image"
-                                sizes="(max-width: 300px) 100vw"
-                                />
-                            <p className="z-20 text-3xl text-green-400 font-bold ">CONFIRM</p>
-                        </div>
-                        <div className="w-[85%] h-[35%] relative flex items-center justify-center hover:scale-110 transition-all cursor-pointer"
-                            onClick={onclick}>
-                            <Image
-                                src={buttonImage}
-                                fill
-                                alt="Button image"
-                                sizes="(max-width: 300px) 100vw"
-                                />
-                            <p className="z-20 text-3xl text-medievalRed font-bold">DECLINE</p>
-                        </div>
-                    </div>
 
+                    <p className="text-[34px] text-center z-10 text-white">Are you sure you want to buy the
+                        <span className="text-orange-400"> {product?.name}</span> for <span className="text-orange-400">{product?.value} g</span> ?</p>
+                    <div className="w-[95%] h-[68%] flex justify-around items-center ml-[3%]">
+                        {/* DIV PARA IMAGEN Y BOTONES */}
+                        <div className="w-[34%] h-[90%] relative">
+                            <Image
+                                src={product?.image!}
+                                fill
+                                alt="item_image"
+                                sizes="(max-width: 300px) 100vw"
+                            />
+
+                        </div>
+                        <div className="w-[50%] h-[90%] flex flex-col items-center justify-center">
+                            <div className="w-[85%] h-[35%] relative mb-[12%] flex items-center justify-center hover:scale-110 transition-all cursor-pointer"
+                                data-testid={'ConfirmButton'}
+                                onClick={buyButtonHandler}>
+                                <Image
+                                    src={buttonImage}
+                                    fill
+                                    alt="Button image"
+                                    sizes="(max-width: 300px) 100vw"
+                                />
+                                <p className="z-20 text-3xl text-green-400 font-bold ">CONFIRM</p>
+                            </div>
+                            <div className="w-[85%] h-[35%] relative flex items-center justify-center hover:scale-110 transition-all cursor-pointer"
+                                onClick={onclick}>
+                                <Image
+                                    src={buttonImage}
+                                    fill
+                                    alt="Button image"
+                                    sizes="(max-width: 300px) 100vw"
+                                />
+                                <p className="z-20 text-3xl text-medievalRed font-bold">DECLINE</p>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
     )
-}   
+}
 
 export default BuyingModal;
--- src/components/shop/Card/Components/CardLeftContainer.tsx
+++ src/components/shop/Card/Components/CardLeftContainer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 interface CardLeftContainerProps {
-    min_lvl: number | null ;
+    min_lvl: number | null;
     value: number;
     image: string;
     name: string;
@@ -15,16 +15,16 @@ interface CardLeftContainerProps {
     player: Player
 }
 
-const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onClickBuy, onClickAddToCart, player} : CardLeftContainerProps) => {
+const CardLeftContainer = ({ min_lvl, value, image, name, description, _id, onClickBuy, onClickAddToCart, player }: CardLeftContainerProps) => {
     const [isDisableButton, setIsDisableButton] = useState<boolean>(false);
 
     useEffect(() => {
-        if(player.gold < value || player.level < min_lvl!){
+        if (player.gold < value || player.level < min_lvl!) {
             setIsDisableButton(true);
-        }else{
+        } else {
             setIsDisableButton(false);
         }
-        
+
     }, [player.gold]);
 
     let icon = '/images/icons/up.png';
@@ -33,25 +33,25 @@ const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onCli
     const minLevelClass = player.level >= (min_lvl || 0) ? 'text-green-400' : 'text-red-600';
 
     return (
-            <div className="flex flex-col relative w-[65%] h-[100%] items-center">
+        <div className="flex flex-col relative w-[65%] h-[100%] items-center">
             {/* DIV LEVEL */}
             <div className="z-10 relative flex flex-row justify-around items-center w-[100%] h-[16%]">
                 <div className={` text-[35px] font-bold w-[20%] pr-[6%] ${minLevelClass}`}
                     data-testid={`min_lvl_${_id}`}>{min_lvl}</div>
-                    <div className="flex justify-around w-[36%] h-[75%] mt-5 items-center text-right">
-                        <p className="text-white w-[65%] text-[25px] font-bold mr-1"
-                            data-testid={`product_value_${_id}`}>{value}</p>
-                        <div className="w-[35%] h-[55%] mt-1 mr-1">
-                            <div className="relative w-full h-full">
-                                    <Image
-                                    src="/images/shop/CoinsIcon.png"
-                                    alt="Gold"
-                                    fill
-                                    sizes='(max-width: 50px) 100vw'
-                                    />
-                            </div>
+                <div className="flex justify-around w-[36%] h-[75%] mt-5 items-center text-right">
+                    <p className="text-white w-[65%] text-[25px] font-bold mr-1"
+                        data-testid={`product_value_${_id}`}>{value}</p>
+                    <div className="w-[35%] h-[55%] mt-1 mr-1">
+                        <div className="relative w-full h-full">
+                            <Image
+                                src="/images/shop/CoinsIcon.png"
+                                alt="Gold"
+                                fill
+                                sizes='(max-width: 50px) 100vw'
+                            />
                         </div>
                     </div>
+                </div>
                 <Image
                     src={icon}
                     width={128}
@@ -63,9 +63,9 @@ const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onCli
             {/* IMAGE DIV */}
             <div className="flex-col flex justify-center items-center h-[45%] w-[65%] mt-0.5">
                 <div className="h-[100%] w-[100%] flex relative items-center justify-center rounded-lg"
-                    > 
-                    <Image 
-                        src={image} 
+                >
+                    <Image
+                        src={image}
                         alt="Equipment Image"
                         fill
                         sizes='(max-width: 570px) 100vw'
@@ -79,30 +79,28 @@ const CardLeftContainer = ({min_lvl, value, image, name, description, _id, onCli
                 <p data-testid={`product_${name}`} >{name}</p>
             </div>
             <div className="h-[14%] text-[22px] flex content-center justify-center overflow-hidden w-[87%]">
-                <p className="line-clamp-none overflow-y-auto " 
+                <p className="line-clamp-none overflow-y-auto "
                     style={{
                         overflowY: "scroll",
                         scrollbarWidth: "none",
                         msOverflowStyle: "none",
-                    }}> 
-                {description}</p>
+                    }}>
+                    {description}</p>
             </div>
             <div className="h-[12%] w-[100%] flex justify-around">
-                <button className="text-white text-xl ml-5 w-[36%] h-[90%] rounded-full flex items-center justify-center" 
+                <button className="text-white text-xl ml-5 w-[36%] h-[90%] rounded-full flex items-center justify-center"
                     onClick={onClickAddToCart}
                     id={_id}
                     data-testid={`add_basket_${_id}`}
-                    disabled={isDisableButton}
-                    >
-                        Add Basket
-                    </button>
-                <button className="text-white text-xl mr-6 w-[36%] h-[90%] rounded-full flex items-center justify-center " 
+                >
+                    Add Basket
+                </button>
+                <button className="text-white text-xl mr-6 w-[36%] h-[90%] rounded-full flex items-center justify-center "
                     onClick={onClickBuy}
                     id={`buy_button_${_id}`}
                     data-testid={`buy_${_id}`}
-                    disabled={isDisableButton}
-                    >
-                        Buy
+                >
+                    Buy
                 </button>
             </div>
         </div>
