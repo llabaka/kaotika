bb3e060e4d3a9402c0be36b33ded6c55815ee590
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-11 10:53:16 +0100

    [ER6-312][ADD] Player gold test. Passing it

--- src/__tests__/FrontendTests/Buy.test.tsx
+++ src/__tests__/FrontendTests/Buy.test.tsx
@@ -7,6 +7,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { mockPlayer } from '../__mocks__/mockPlayer';
 import { mockProduct } from '../__mocks__/mockProduct';
 import { mockShopTooltip } from '../__mocks__/mockShopTooltip';
+import PlayerStatsButtons from '@/components/shop/PlayerStatsButtons';
 
 global.fetch = jest.fn() as jest.Mock;
 
@@ -22,26 +23,26 @@ describe('Buy button functionality', () => {
   it('should open buying modal when clicking buy button', () => {
 
     ////////////////////////// ARRANGE //////////////////////////
-    
+
     (fetch as jest.Mock).mockResolvedValueOnce({
       ok: true,
       json: async () => ({
         data: { ...mockPlayer, gold: 3819 }, // Simulate updated player data
-        }),
-      });
-      
-      // Mock props
-      const mockCartProducts: Product[] = []; // Empty array for initial state
-      const mockSetCartProducts = jest.fn(); // Mocked setter function
-      const mockOnClickBuy = jest.fn();
-      const mockOnClick = jest.fn();
-      const mockSetProduct = jest.fn();
-      const mockSetPlayer = jest.fn();
-      const mockSetHaveBuy = jest.fn();
-      const mockSetShopTooltips = jest.fn();
-      
-      // Render the component
-      render(
+      }),
+    });
+
+    // Mock props
+    const mockCartProducts: Product[] = []; // Empty array for initial state
+    const mockSetCartProducts = jest.fn(); // Mocked setter function
+    const mockOnClickBuy = jest.fn();
+    const mockOnClick = jest.fn();
+    const mockSetProduct = jest.fn();
+    const mockSetPlayer = jest.fn();
+    const mockSetHaveBuy = jest.fn();
+    const mockSetShopTooltips = jest.fn();
+
+    // Render the component
+    render(
       <CardItem
         card={mockProduct}
         onClickBuy={mockOnClickBuy}
@@ -52,40 +53,9 @@ describe('Buy button functionality', () => {
         setShopTooltips={mockSetShopTooltips}
       />
     );
-    
-      // Render the BuyingModal component
-      render(
-        <BuyingModal
-          product={mockProduct}
-          onclick={mockOnClick}
-          player={mockPlayer}
-          setPlayer={mockSetPlayer}
-          setHaveBuy={mockSetHaveBuy}
-          setShopTooltips={mockSetShopTooltips}
-        />
-      );
 
-      ////////////////////////// ACT //////////////////////////
-  
-      const buyingButton = screen.getByText('Buy');
-      fireEvent.click(buyingButton);
-
-      ////////////////////////// ASSERT //////////////////////////
-      expect(screen.getByTestId('buying-modal')).toBeInTheDocument();
-      expect(screen.getByTestId('ConfirmButton')).toBeInTheDocument();
-    });
-
-    it('should show item bought message when clicking confirm button', () => {
-
-      // Mock props
-      const mockOnClick = jest.fn();
-      const mockSetPlayer = jest.fn();
-      const mockSetHaveBuy = jest.fn();
-      const mockSetShopTooltips = jest.fn();
-      const mockIndex = 1;
-
-      // Render the BuyingModal component
-      render(
+    // Render the BuyingModal component
+    render(
       <BuyingModal
         product={mockProduct}
         onclick={mockOnClick}
@@ -96,20 +66,83 @@ describe('Buy button functionality', () => {
       />
     );
 
-      //Render tooltip
-      render(
+    ////////////////////////// ACT //////////////////////////
+
+    const buyingButton = screen.getByText('Buy');
+    fireEvent.click(buyingButton);
+
+    ////////////////////////// ASSERT //////////////////////////
+    expect(screen.getByTestId('buying-modal')).toBeInTheDocument();
+    expect(screen.getByTestId('ConfirmButton')).toBeInTheDocument();
+  });
+
+  it('should show item bought message when clicking confirm button', () => {
+
+    // Mock props
+    const mockOnClick = jest.fn();
+    const mockSetPlayer = jest.fn();
+    const mockSetHaveBuy = jest.fn();
+    const mockSetShopTooltips = jest.fn();
+    const mockIndex = 1;
+
+    // Render the BuyingModal component
+    render(
+      <BuyingModal
+        product={mockProduct}
+        onclick={mockOnClick}
+        player={mockPlayer}
+        setPlayer={mockSetPlayer}
+        setHaveBuy={mockSetHaveBuy}
+        setShopTooltips={mockSetShopTooltips}
+      />
+    );
+
+    //Render tooltip
+    render(
       <ShopTooltip
         image={mockShopTooltip.image}
-        itemName={mockShopTooltip.itemName} 
+        itemName={mockShopTooltip.itemName}
         key={mockIndex}
         action={mockShopTooltip.action}
       />
     );
-    
+
     const confirmButton = screen.getByTestId('ConfirmButton');
     fireEvent.click(confirmButton);
 
     expect(screen.getByTestId('ShopTooltip')).toBeInTheDocument();
     expect(screen.getByTestId('ShopTooltip')).toHaveTextContent('You have bought Knife !!');
   })
+
+  it('should show updated gold from player in screen', async () => {
+
+    ////////////////////////// ARRANGE //////////////////////////
+
+    // Mock state and setState for the player
+    let player = { ...mockPlayer };
+
+    const mockSetPlayer = jest.fn((newPlayer) => {
+      player = newPlayer;
+    });
+
+    const { rerender } =
+      render(
+        <PlayerStatsButtons player={player} />
+      );
+
+    ////////////////////////// ACT //////////////////////////
+
+    // Simulate incoming gold is changed because buying and set to player
+    const updatedPlayer = { ...mockPlayer, gold: 3819 };
+    mockSetPlayer(updatedPlayer);
+
+    // Re-render the component with updated player
+    rerender(<PlayerStatsButtons player={updatedPlayer} />);
+
+    ////////////////////////// ASSERT //////////////////////////
+
+    // Verify the updated gold is displayed
+    expect(screen.getByTestId('PlayerGold')).toBeInTheDocument();
+    expect(screen.getByTestId('PlayerGold')).toHaveTextContent('3819');
+  });
 });
