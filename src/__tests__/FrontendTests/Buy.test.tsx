c6c1d9e7d6420bfae6dd50365928ad2013c0636d
llabaka <lander.labaka@ikasle.aeg.eus>
2024-12-11 09:33:14 +0100

    [ER5-312][ADD] Confirm button shows tooltip test pass

--- src/__tests__/FrontendTests/Buy.test.tsx
+++ src/__tests__/FrontendTests/Buy.test.tsx
@@ -1,97 +1,114 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import CardItem from '@/components/shop/CardItem';
 import { Product } from '@/_common/interfaces/shop/Product';
+import BuyingModal from '@/components/shop/BuyingModal/BuyingModal';
+import CardItem from '@/components/shop/CardItem';
+import ShopTooltip from '@/components/tooltips/ShopTooltip';
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { mockPlayer } from '../__mocks__/mockPlayer';
 import { mockProduct } from '../__mocks__/mockProduct';
-import BuyingModal from '@/components/shop/BuyingModal/BuyingModal';
-import ShopTooltip from '@/components/tooltips/ShopTooltip';
 import { mockShopTooltip } from '../__mocks__/mockShopTooltip';
+
+global.fetch = jest.fn() as jest.Mock;
+
+beforeEach(() => {
+  (fetch as jest.Mock).mockClear(); // Clear previous mock data before each test
+});
+
+afterAll(async () => {
+  jest.restoreAllMocks(); // Restaurar todos los mocks
+});
 
 describe('Buy button functionality', () => {
     it('should open buying modal when clicking buy button', () => {
 
-            ////////////////////////// ARRANGE //////////////////////////
-
-        // Mock props
-        const mockCartProducts: Product[] = []; // Empty array for initial state
-        const mockSetCartProducts = jest.fn(); // Mocked setter function
-        const mockOnClickBuy = jest.fn();
-        const mockOnClick = jest.fn();
-        const mockSetProduct = jest.fn();
-        const mockSetPlayer = jest.fn();
-        const mockSetHaveBuy = jest.fn();
-        const mockSetShopTooltips = jest.fn;
+    ////////////////////////// ARRANGE //////////////////////////
     
-        // Render the component
-        render(
-            <CardItem
-                card={mockProduct}
-                onClickBuy={mockOnClickBuy}
-                setProduct={mockSetProduct}
-                setCartProducts={mockSetCartProducts}
-                cartProducts={mockCartProducts}
-                player={mockPlayer}
-                setShopTooltips={mockSetShopTooltips}
-            />
-        );
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        data: { ...mockPlayer, gold: 3819 }, // Simulate updated player data
+        }),
+      });
+      
+      // Mock props
+      const mockCartProducts: Product[] = []; // Empty array for initial state
+      const mockSetCartProducts = jest.fn(); // Mocked setter function
+      const mockOnClickBuy = jest.fn();
+      const mockOnClick = jest.fn();
+      const mockSetProduct = jest.fn();
+      const mockSetPlayer = jest.fn();
+      const mockSetHaveBuy = jest.fn();
+      const mockSetShopTooltips = jest.fn();
+      
+      // Render the component
+      render(
+      <CardItem
+        card={mockProduct}
+        onClickBuy={mockOnClickBuy}
+        setProduct={mockSetProduct}
+        setCartProducts={mockSetCartProducts}
+        cartProducts={mockCartProducts}
+        player={mockPlayer}
+        setShopTooltips={mockSetShopTooltips}
+      />
+    );
 
-        // Render the BuyingModal component
-        render(
-            <BuyingModal
-                product={mockProduct}
-                onclick={mockOnClick}
-                player={mockPlayer}
-                setPlayer={mockSetPlayer}
-                setHaveBuy={mockSetHaveBuy}
-                setShopTooltips={mockSetShopTooltips}
-            />
-        );
+    // Render the BuyingModal component
+    render(
+      <BuyingModal
+        product={mockProduct}
+        onclick={mockOnClick}
+        player={mockPlayer}
+        setPlayer={mockSetPlayer}
+        setHaveBuy={mockSetHaveBuy}
+        setShopTooltips={mockSetShopTooltips}
+      />
+    );
 
-        ////////////////////////// ACT //////////////////////////
-    
-        const buyingButton = screen.getByText('Buy');
-        fireEvent.click(buyingButton);
+      ////////////////////////// ACT //////////////////////////
+  
+      const buyingButton = screen.getByText('Buy');
+      fireEvent.click(buyingButton);
 
-        ////////////////////////// ASSERT //////////////////////////
-        expect(screen.getByTestId('buying-modal')).toBeInTheDocument();
-        expect(screen.getByTestId('ConfirmButton')).toBeInTheDocument();
+      ////////////////////////// ASSERT //////////////////////////
+      expect(screen.getByTestId('buying-modal')).toBeInTheDocument();
+      expect(screen.getByTestId('ConfirmButton')).toBeInTheDocument();
     });
 
     it('should show item bought message when clicking confirm button', () => {
 
-        // Mock props
-        const mockOnClick = jest.fn();
-        const mockSetPlayer = jest.fn();
-        const mockSetHaveBuy = jest.fn();
-        const mockSetShopTooltips = jest.fn();
-        const mockIndex = 1;
+      // Mock props
+      const mockOnClick = jest.fn();
+      const mockSetPlayer = jest.fn();
+      const mockSetHaveBuy = jest.fn();
+      const mockSetShopTooltips = jest.fn();
+      const mockIndex = 1;
 
-        // Render the BuyingModal component
-        render(
-            <BuyingModal
-                product={mockProduct}
-                onclick={mockOnClick}
-                player={mockPlayer}
-                setPlayer={mockSetPlayer}
-                setHaveBuy={mockSetHaveBuy}
-                setShopTooltips={mockSetShopTooltips}
-            />
-        );
+      // Render the BuyingModal component
+      render(
+          <BuyingModal
+              product={mockProduct}
+              onclick={mockOnClick}
+              player={mockPlayer}
+              setPlayer={mockSetPlayer}
+              setHaveBuy={mockSetHaveBuy}
+              setShopTooltips={mockSetShopTooltips}
+          />
+      );
 
-        //Render tooltip
-        render(
-            <ShopTooltip
-            image={mockShopTooltip.image}
-            itemName={mockShopTooltip.itemName}
-            key={mockIndex}
-            action={mockShopTooltip.action}/>
-        );
+      //Render tooltip
+      render(
+          <ShopTooltip
+          image={mockShopTooltip.image}
+          itemName={mockShopTooltip.itemName} 
+          key={mockIndex}
+          action={mockShopTooltip.action}/>
+      );
 
-        const confirmButton = screen.getByTestId('ConfirmButton');
-        fireEvent.click(confirmButton);
+      const confirmButton = screen.getByTestId('ConfirmButton');
+      fireEvent.click(confirmButton);
 
-        //expect(screen.getByTestId('ShopTooltip')).toBeInTheDocument();
+      expect(screen.getByTestId('ShopTooltip')).toBeInTheDocument();
     })
 
 });
--- src/components/tooltips/ShopTooltip.tsx
+++ src/components/tooltips/ShopTooltip.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import { ShopTooltipProps } from "@/_common/interfaces/shop/ShopTooltip";
+import Image from "next/image";
 
 
 const ShopTooltip = ({image, action, itemName} : ShopTooltipProps ) => {
 
     return (
-            <div className="h-32 bg-black bg-opacity-90 flex items-center justify-around relative mt-2" data-test-id='ShopTooltip'>
+            <div className="h-32 bg-black bg-opacity-90 flex items-center justify-around relative mt-2" data-testid={'ShopTooltip'}>
                 <div className="w-[30%] h-[80%] relative">
                     <Image
                         src={image}
