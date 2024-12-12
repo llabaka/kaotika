a2e718d01ecb4cbe5f0a1b2ee6c0dc2074b82d73
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-12-12 13:41:42 +0100

    [STAGING][UPDATE] Tab in EmptySeparator

--- src/components/ProductVerticalEmptySeparator.tsx
+++ src/components/ProductVerticalEmptySeparator.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 
 const ProductEmptyVerticalSeparator = () => {
 
-    return(
+  return (
     <div className="flex w-[2%] h-[23rem] bg-transparent text-white">
-         <div className="relative w-[60%] left-1 h-[23rem]">
-
-        </div>
-    </div>    )
+      <div className="relative w-[60%] left-1 h-[23rem]">
+      </div>
+    </div>)
 }
 
 export default ProductEmptyVerticalSeparator;
